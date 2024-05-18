5acb1422b36fea88ff2534ae1634ee56db9fc3d2
rel856 <134518059+rel856@users.noreply.github.com>
2024-05-18 19:05:12 +0200

    Create mandelbrot_mpi.c

---
+++ mandelbrot_mpi.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <complex.h>
+#include <sys/time.h>
+#include <mpi.h>
+
+void mandelbrot(int m, int n, double x1, double x2, double y1, double y2,
+                int max_iter, int *picture /* out */, int rank, int size)
+{
+    int i_start = rank * m / size;
+    int i_end = (rank + 1) * m / size;
+
+    for (int i = i_start; i < i_end; i++) {
+        for (int j = 0; j < n; j++) {
+            double complex c =  x1 + (x2 - x1) * i / m + 
+                               (y1 + (y2 - y1) * j / n) * I;
+            double complex z = 0;
+            int t = 0;
+
+            while ((cabs(z) <= 2) && t < max_iter) {
+                z = z * z + c;
+                t++;
+            }
+
+            picture[(i - i_start) * n + j] = t;
+        }
+    }
+}
+
+int main(int argc, char **argv)
+{
+    MPI_Init(&argc, &argv);
+
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    if (argc != 8) {
+        printf("Usage: %s M N MAX_ITER x1 x2 y1 y2\n"
+                "\tcreates an M x N pixel picture of [x1, x2] x [y1, y2]\n"
+                "\tescaping after MAX_ITER iterations\n", argv[0]);
+        MPI_Finalize();
+        return EXIT_FAILURE;
+    }
+
+    //Store variables in all processes
+    int m;
+    int n;
+    int max_iter;
+    double x1;
+    double x2;
+    double y1;
+    double y2;
+
+    //But only root process needs to read parameters
+    if(rank == 0) {
+        m        = atoi(argv[1]);
+        n        = atoi(argv[2]);
+        max_iter = atoi(argv[3]);
+        x1       = atof(argv[4]);
+        x2       = atof(argv[5]);
+        y1       = atof(argv[6]);
+        y2       = atof(argv[7]);
+    }
+
+    int *picture = malloc(m * n * sizeof(int));
+
+    struct timeval tv1, tv2;
+    gettimeofday(&tv1, NULL);
+
+    /* Distribute blocks to processes (assume 4 for now), compute them */
+    // Broadcast the parameters to all processes
+    MPI_Bcast(&m, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&max_iter, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&x1, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&x2, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&y1, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&y2, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+ 
+    // Call the mandelbrot function on each process
+    printf("rank %d, received param: %i %i %i %f %f %f %f\n", rank, m, n ,max_iter, x1, x2, y1, y2);
+    mandelbrot(m, n, x1, x2, y1, y2, max_iter, picture, rank, size);
+    int *full_picture = NULL;
+
+    if (rank == 0) {
+        full_picture = malloc(m * n * sizeof(int));
+    }
+
+
+    int i_start = rank * m / size;
+    int i_end = (rank + 1) * m / size;
+    MPI_Gather(picture + i_start * n, (i_end - i_start) * n, MPI_INT, 
+        full_picture, (i_end - i_start) * n, MPI_INT, 0, MPI_COMM_WORLD);
+    if (rank == 0) {
+        /* Write pgm to stderr. */
+        fprintf(stderr, "P2\n");
+        fprintf(stderr, "%d %d\n", m, n);
+        fprintf(stderr, "%d\n", max_iter);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                fprintf(stderr, "%d ", full_picture[i * n + j]);
+            }
+            fprintf(stderr, "\n");
+        }
+    }
+
+
+    free(picture);
+
+    MPI_Finalize();
+
+    return EXIT_SUCCESS;
+}
